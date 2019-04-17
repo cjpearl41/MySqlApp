09cc22e54acdffdba493b69706f87d9cdfd2f341
cjpearl41 <cjpearl41@gmail.com>
2019-04-17 12:28:17 -0600

    Initial Commit, work in progress

---
+++ app/src/main/java/com/example/mysqlexampleproject/DbStrings.java
@@ -0,0 +1,11 @@
+package com.example.mysqlexampleproject;
+
+public class DbStrings {
+
+
+    static final String DATABASE_URL="gator4268.hostgator.com";
+    static final String DATABASE_NAME = "mmcclend_fruity";
+    static final String USERNAME = "mmcclend_fruity";
+    static final String PASSWORD = "fruittoor";
+
+}
